a9536232aa329e873403c5e12fafdf191a74c772
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-06-07 12:36:10 +0200

    strapi upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '574073f713155f89b26138d15438119e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a0d5bb53-0ebb-47fc-b711-b21db32ac5a0'
+};
